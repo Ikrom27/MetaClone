ea127c0496bd4006b9ef66fd72285c93f88c1db9
Ikrom27 <ikrom.hasanbaev.27@gmail.com>
2025-07-12 23:35:30 +0300

    tests added

--- service-auth/src/main/java/ru/metaclone/service_auth/repository/AuthRepository.java
+++ service-auth/src/main/java/ru/metaclone/service_auth/repository/AuthRepository.java
@@ -1,8 +1,8 @@
 package ru.metaclone.service_auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.metaclone.service_auth.model.entity.RegisteredUserEntity;
+import ru.metaclone.service_auth.model.entity.CredentialsEntity;
 
-public interface AuthRepository extends JpaRepository<RegisteredUserEntity, Long> {
-    RegisteredUserEntity findByLogin(String login);
+public interface AuthRepository extends JpaRepository<CredentialsEntity, Long> {
+    CredentialsEntity findByLogin(String login);
 }
