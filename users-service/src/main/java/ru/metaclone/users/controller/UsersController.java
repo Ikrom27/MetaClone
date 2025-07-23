0c1d93b457d15b116075c7533a1870d5c6016b0a
Ikrom27 <ikrom.hasanbaev.27@gmail.com>
2025-07-23 21:40:08 +0300

    [USERS] - controller update

--- users-service/src/main/java/ru/metaclone/users/controller/UsersController.java
+++ users-service/src/main/java/ru/metaclone/users/controller/UsersController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +24,7 @@ public class UsersController {
         this.usersService = usersService;
     }
 
-    @PostMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<SaveUserResponse> update(@Validated @RequestBody SaveUserDetailsRequest saveUserDetailsRequest,
                                                  @PathVariable("id") Long userId) {
         var body = usersService.saveUserRequest(userId, saveUserDetailsRequest);
