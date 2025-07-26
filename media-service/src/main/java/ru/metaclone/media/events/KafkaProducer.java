f7da69398526ad73706862c467d1f19cabb1983f
Ikrom27 <ikrom.hasanbaev.27@gmail.com>
2025-07-26 14:04:59 +0300

    [MEDIA] - kafka producer added

--- media-service/src/main/java/ru/metaclone/media/events/KafkaProducer.java
+++ media-service/src/main/java/ru/metaclone/media/events/KafkaProducer.java
@@ -1,10 +1,27 @@
 package ru.metaclone.media.events;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import ru.metaclone.media.data.events.UserAvatarUpdatedEvent;
 
 @Service
 public class KafkaProducer {
+
+    @Value("${kafka.topic.avatar-update}")
+    private String AVATAR_UPDATE_TOPIC;
+
+    private final KafkaTemplate<String, UserAvatarUpdatedEvent> kafka;
+
+    public KafkaProducer(KafkaTemplate<String, UserAvatarUpdatedEvent> kafka) {
+        this.kafka = kafka;
+    }
+
     public void produceAvatarUpdate(Long userId, String url) {
-        System.out.println("AVATAR UPDATED " + url);
+        try {
+            kafka.send(AVATAR_UPDATE_TOPIC, new UserAvatarUpdatedEvent(userId, url));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send message ", e);
+        }
     }
 }
